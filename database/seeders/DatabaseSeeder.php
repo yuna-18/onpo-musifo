d836a03ade9bbdc7c153205f73598f3b55174664
yuna-18 <huraigon10@gmail.com>
2025-03-25 03:52:38 +0900

    マイグレーションファイルエラー修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,28 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
+    // ここにまとめて書いておく！
+    $this->call([
+      AreasSeeder::class,
+      // CitiesSeeder::class,
+      // UsersSeeder::class,
+      MusicCategoriesSeeder::class,
+      MusicInstCategoriesSeeder::class,
+      MusicCategoriesMusicInstCategoriesSeeder::class,
+      MusicInstsSeeder::class,
+      SubareasSeeder::class,
+      // 他にもあれば追加
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    // User::factory()->create([
+    //   'name' => 'Test User',
+    //   'email' => 'test@example.com',
+    // ]);
+  }
 }
