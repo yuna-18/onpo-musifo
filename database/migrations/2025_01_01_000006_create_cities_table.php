c338361fe65fd48ed3987898b9b73eab9064b80e
yuna-18 <huraigon10@gmail.com>
2025-03-29 07:24:19 +0900

    Bookmarks titleカラム追加

--- database/migrations/2025_01_01_000006_create_cities_table.php
+++ database/migrations/2025_01_01_000006_create_cities_table.php
@@ -22,9 +22,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::table('cities', function (Blueprint $table) {
-            $table->dropConstrainedForeignId('sub_area_id');
-        });
         Schema::dropIfExists('cities');
     }
 };
